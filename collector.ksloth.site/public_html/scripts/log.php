a87a1751f2fd24ba3f2aceb4ddce05c9612dc281
KaronLan <166756760+KaronLuoS@users.noreply.github.com>
2026-08-29 17:38:55 -0700

    HW3 Part3 Attempt7 delete debug script

--- collector.ksloth.site/public_html/scripts/log.php
+++ collector.ksloth.site/public_html/scripts/log.php
@@ -7,8 +7,8 @@
 
 declare(strict_types=1);
 
-ini_set('display_errors', '1');
-error_reporting(E_ALL);
+// ini_set('display_errors', '1');
+// error_reporting(E_ALL);
 
 $config = require dirname(__DIR__, 2) . '/database/config.php';
 
