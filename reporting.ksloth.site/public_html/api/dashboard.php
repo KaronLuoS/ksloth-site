2221554c98774dc785ddff0de2157499819c2d96
KaronLan <166756760+KaronLuoS@users.noreply.github.com>
2026-08-31 21:39:30 -0700

    HW4 add filler dashboard.php for testing

---
+++ reporting.ksloth.site/public_html/api/dashboard.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+    <h1>you reached dashboard.php</h1>
+</body>
+</html>';
+?>
